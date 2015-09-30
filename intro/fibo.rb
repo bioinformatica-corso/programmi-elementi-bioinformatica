1d51268daa680740d95fdf396d15767bba3efc31
Gianluca Della Vedova <gianluca@dellavedova.org>
2015-09-30 10:58:14 +0200

    Simplified assignment

--- intro/fibo.rb
+++ intro/fibo.rb
@@ -5,7 +5,9 @@
 ARGV.each do |limit|
   fib = Array.new()
   limit.to_i.times do |x|
-    fib[x] = if x <= 1 then 1 else fib[x] = fib[x - 1] + fib[x - 2] end
+    fib[x] = if x <= 1 then 1 else fib[x - 1] + fib[x - 2] end
   end
   puts fib
 end
+
+
