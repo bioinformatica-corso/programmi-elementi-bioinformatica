4773141b2579d5b6f8465abc3f3d6a0460600730
Gianluca Della Vedova <gianluca@dellavedova.org>
2015-10-12 05:31:25 +0200

    More tests, data in new dir

--- karp-rabin/run-tests.sh
+++ karp-rabin/run-tests.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 dir="tests"
-test -f "$dir"/testo.1.fa.gz || wget -O "$dir"/testo.1.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/chromosomes/chrI.fa.gz
-test -f "$dir"/testo.2.fa.gz || wget -O "$dir"/testo.2.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr1.fa.gz
+datadir="${dir}/.data/"
+test -d "$datadir" || mkdir -p "$datadir"
+test -f "$datadir"/testo.1.fa.gz || wget -O "$datadir"/testo.1.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/chromosomes/chrI.fa.gz
+test -f "$datadir"/testo.2.fa.gz || wget -O "$datadir"/testo.2.fa.gz http://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr1.fa.gz
 
 for id in $(seq 2)
 do
-    text="${dir}/testo.${id}.fa.gz"
+    text="${datadir}/testo.${id}.fa.gz"
     test -f "$text" || exit 1
+    test -f "$dir"/output || mkdir -p "$dir"/output
     for t in ${dir}/input/*
     do
 	p=$(cat "$t")
